4e982413bd32edd74e05f2a2a503db96eb245904
Tiqur <Tiqur7@gmail.com>
2020-12-23 00:40:30 -0800

    Basic file upload / media storage

--- fileo/fileo/urls.py
+++ fileo/fileo/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -23,4 +25,4 @@ urlpatterns = [
     path('download/', views.download, name='download'),
     path('admin/', admin.site.urls, name='admin'),
     path('account/', include('django.contrib.auth.urls'), name='account')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- fileo/fileo/views.py
+++ fileo/fileo/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.decorators import login_required
 
 def index(request):
@@ -6,9 +8,17 @@ def index(request):
 
 @login_required(login_url='login')
 def myfiles(request):
-    context = {'files': list(range(1, 100))}
+    print("Request: " + request.method)
+    context = {'files': list(range(1, 20))}
+    if request.method == 'POST' and request.FILES['upload_file']:
+        file = request.FILES.get('upload_file')
+        fs = FileSystemStorage()
+        filename = fs.save(file.name, file)
+        uploaded_file_url = fs.url(filename)
+        context['uploaded_file_url'] = uploaded_file_url
+
     return render(request, 'fileo/myfiles.html', context)
 
 def download(request):
     context = {'file_path': '/yeet.txt'}
-    return render(request, 'fileo/download.html', context)
+
